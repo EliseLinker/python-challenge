edfb8c656a616b5f9f33f8ae5b1f64edc7c60674
Elise Linker <eliselinker@me.com>
2018-02-14 15:40:36 -0600

    committing changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -31,21 +31,30 @@
 #(your boss is going to give you more of these -- so your script has to work for the ones to come). 
 #In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
+#import libraries
 import os 
 import csv 
 from collections import Counter 
 
+#input csv file path
 csvpath = os.path.join('Data','budget_data_1.csv')
 
+#initialize variables 
 date = []
 budget_data = [] 
 total_revenue = 0
 hold_date = ''
 date_counter = 0 
 prev_revenue = 0
-revenue_change = []
+revenue_change = 0
 hold_revenue_change = 0 
+total_revenue_change = 0 
+greatist_decrease_amt = 0 
+greatist_decrease_Date = ""
+greatist_increase_amt = 0 
+greatist_increase_date = ""
 
+#open csv file and read reach record 
 with open(csvpath, newline="") as csvfile:
     csvreader= csv.reader(csvfile, delimiter=",")
     next(csvfile)
@@ -56,50 +65,58 @@ with open(csvpath, newline="") as csvfile:
         budget_revenue = row[1]
 
     #Tally number of months in budget
-        #print(date_list)
-        #print(revenue_list)
         if date_counter == 0:
             hold_date = budget_date 
             date_counter += 1
-            prev_revenue = budget_revenue 
+            prev_revenue = budget_revenue
         elif hold_date != budget_date:
             hold_date = budget_date 
+            #increment date counter
             date_counter += 1 
-            #print(prev_revenue)
-            revenue_change.append (budget_revenue) #int(budget_revenue) - int(prev_revenue)
-            #prev_revenue = budget_revenue
+            #Calculate total Revenue change 
+            total_revenue_change = total_revenue_change + (int(budget_revenue) - int(prev_revenue)) 
+            if total_revenue_change < greatist_decrease_amt:
+                greatist_decrease_amt = total_revenue_change
+                greatist_decrease_date = budget_date 
+                print("decrease " + str(greatist_decrease_date) + " " + str(greatist_decrease_amt))
+            elif total_revenue_change > greatist_increase_amt:
+                greatist_increase_amt = total_revenue_change
+                greatist_increase_date = budget_date 
+                print("inrease " + str(greatist_increase_date) + " " + str(greatist_increase_amt))
+                
+            #populate previous revenue 
+            prev_revenue = budget_revenue
+        
+    #print("total revenue change: " + str(total_revenue_change))
 
-        #print(date_counter)
-        #print(prev_revenue)
-        #print(revenue_list)
-            
-        #for i in range(0,len(revenue_change)):
-        print(revenue_change)
-            #min(revenue_change)
-        #print(type(revenue_change))
+    #Calculate average revenue change     
+    average_revenue_change = total_revenue_change/(date_counter -1)
+    #print("average_revenue_change")
+    #print(average_revenue_change)
 
-        #???    why isnt this working????
-    #total_revenue_change = sum(revenue_change)/len(revenue_change)
-
-
-        #hold_revenue_change + revenue_change
-        #hold_revenue_change = revenue_change 
-
+    
     #Calculate Total_Revenue for the entire budget period 
     total_revenue = total_revenue + int(budget_revenue)
-        #print(str(total_revenue))
+    #print("Total_Revenue")
+    #print(str(total_revenue))
 
-    #* The average change in revenue between months over the entire period
-    #Calculate the change between months
-    # new month - old month = change 
-    
-    #    if prev_revenue != 0.00:
-    #        revenue_change = revenue_list - prev_revenue
+    print("----------------------------")
+    print("Financial Analysis")
+    print("----------------------------")
+    print("Total Months: " + str(date_counter))
+    print("Total_Revenue: " + str(total_revenue))
+    print("Average Revenue Change:" + str(average_revenue_change))
+    print("Greatist Increase in Revenue:" + str(greatist_increase_date) + " " + str(greatist_increase_amt))
+    print("Greatist Decrease in Revenue:" + str(greatist_decrease_date) + " " + str(greatist_decrease_amt))
 
-    #hold that data in a list
-
-    #calculate the average on the list 
-
+#```
+#Financial Analysis
+#----------------------------
+#Total Months: 25
+#Total Revenue: $1241412
+#Average Revenue Change: $216825
+#Greatest Increase in Revenue: Sep-16 ($815531)
+#Greatest Decrease in Revenue: Aug-12 ($-652794)
 
 
 
